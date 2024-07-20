295e4385d759f54efa8e9af86ec74e64a4f55e36
Fayi <fayi.pon@gmail.com>
2024-07-20 20:11:29 +0800

    Update TgBotController.php

--- app/Http/Controllers/TgBotController.php
+++ app/Http/Controllers/TgBotController.php
@@ -18,12 +18,36 @@ class TgBotController extends SiteController {
     public function index(Request $request) {
     	
         $this->getRequest($request);
+        $reponse = $this->request;
 
-        $reponse = json_encode($this->request);
+
+        // 判斷信號為做多或空
+        if ($reponse['close'] >= $reponse['open']) {
+            $reponse['type'] = "buy";
+        } else {
+            $reponse['type'] = "sell";
+        }
+
+        $reponse = json_encode($reponse);
 
         $data = Test::create([
             "data" => $reponse
         ]);
+
+        /*
+        {
+        "type":"sell",
+        "ticker":"{{ticker}}",
+        "open":"{{open}}",
+        "close":"{{close}}",
+        "high":"{{high}}",
+        "low":"{{low}}",
+        "volume":"{{volume}}",
+        "time":"{{time}}"
+        }
+        */
+
+
         
         
         file_get_contents("https://api.telegram.org/bot7360641960:AAHeOdSE1MmR5nJU1iiJtP0pM0-W9XEgTOU/sendMessage?chat_id=545205414&text=" . urlencode($reponse));
