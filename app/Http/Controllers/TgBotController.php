c62274184ca47d70b042a1ac48eaa960a8257171
Fayi <fayi.pon@gmail.com>
2024-07-20 20:35:58 +0800

    Update TgBotController.php

--- app/Http/Controllers/TgBotController.php
+++ app/Http/Controllers/TgBotController.php
@@ -25,20 +25,25 @@ class TgBotController extends SiteController {
             "data" => json_encode($this->request)
         ]);
 
+        $default_rate = 0.01;
+
         // 判斷信號為做多或空
         if ($reponse['close'] >= $reponse['open']) {
-            $reponse['type'] = "buy";
+            $reponse['type'] = "做多";
+            $rate = 1 - $default_rate;
+            $reponse['ticker'] = "↗︎ " . $reponse['ticker'];
         } else {
-            $reponse['type'] = "sell";
+            $reponse['type'] = "做空";
+            $rate = 1 + $default_rate;
+            $reponse['ticker'] = "↘︎ " . $reponse['ticker'];
         }
 
         // 組合發送資料
-        $message = $reponse['type'] . "
+        $message = $reponse['ticker'] . " " . $reponse['type'] . "
 =========================
-line 2
-line 3
-line 4
-line 5
+當前價格 : " . $reponse['close'] . "
+建議止損 : " . $reponse['close'] * $rate . " (1%)
+發送時間 : " . date("Y-m-d H:i:s") . "
 ";
         
         file_get_contents("https://api.telegram.org/bot7360641960:AAHeOdSE1MmR5nJU1iiJtP0pM0-W9XEgTOU/sendMessage?chat_id=545205414&text=" . urlencode($reponse));
