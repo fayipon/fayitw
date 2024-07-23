2498e73d64e8f14d45bc68575be30c212fbe77e1
Fayi <fayi.pon@gmail.com>
2024-07-23 22:30:37 +0800

    Update TgBotController.php

--- app/Http/Controllers/TgBotController.php
+++ app/Http/Controllers/TgBotController.php
@@ -22,69 +22,22 @@ class TgBotController extends SiteController {
         $reponse = $this->request;
         $this->record($reponse);
 
-        date_default_timezone_set("Asia/Taipei");
-        $default_rate = 0.01;
-        $mode = 0;
+       // date_default_timezone_set("Asia/Taipei");
+        
+        $ticker  = str_replace([".P"], [""], $reponse['ticker']);
 
-        $time_type = $reponse['type'];
-
-        $reponse['ticker']  = str_replace([".P"], [""], $reponse['ticker']);
-        $ticker_boinnance = $reponse['ticker'];
-
-        // 判斷信號為做多或空
-        if ($reponse['close'] >= $reponse['open']) {
-            $reponse['type'] = "做多";
-            $rate = 1 - $default_rate;
-            $reponse['ticker'] = "↗︎ " . $reponse['ticker'];
-            $mode = 1;
-        } else {
-            $reponse['type'] = "做空";
-            $rate = 1 + $default_rate;
-            $reponse['ticker'] = "↘︎ " . $reponse['ticker'];
-            $mode = 0;
-        }
-
-        // 取得當前資料
-        $result = Stock::where('name',$ticker_boinnance)->first();
-        if ($result === false) {
-            dd($result);
-        }
-
-        // 組合發送資料
-        $this->send($reponse, $time_type, $binance_tradingview);
-
-        // 更新資料
-        if ($time_type == 1) {
-            // 1h線
-            $result = Stock::where('name',$ticker_boinnance)->update([
-                '1h' => $mode,
-                'updated_at' => date("Y-m-d H:i:s")
-            ]);
-            if ($result === false) {
-                dd($result);
-            }
-        } else {
-            // 4h線
-            $result = Stock::where('name',$ticker_boinnance)->update([
-                '4h' => $mode,
-                'updated_at' => date("Y-m-d H:i:s")
-            ]);
-            if ($result === false) {
-                dd($result);
-            }
-        }
-
+        $this->send($reponse, $ticker);
     }
 
     // send message
-    protected function send($reponse,$time_type,$binance_tradingview) {
+    protected function send($reponse, $ticker) {
 
-        $message = $reponse['ticker'] . " " . $time_type . "H " . $reponse['type'] . "
+        $message = $ticker . " " . $reponse['type'] . "
 =========================
 當前價格 : " . $reponse['close'] . "
 發送時間 : " . date("Y-m-d H:i:s") . "
 
-Binance : https://www.binance.com/zh-TC/futures/" . $binance_tradingview . "
+Binance : https://www.binance.com/zh-TC/futures/" . $reponse['ticker'] . "
 ";
         
         // 我自已 測試用
