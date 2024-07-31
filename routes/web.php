1b012bc28cb321a810e80f3d3faff37a30be43b2
Fayi <fayi.pon@gmail.com>
2024-07-31 21:33:31 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -19,4 +19,4 @@ Route::get('/', 'App\Http\Controllers\HomeController@index');
 Route::get('/tg', 'App\Http\Controllers\TgBotController@index');
 Route::post('/tg', 'App\Http\Controllers\TgBotController@index');
 
-Route::post('/tg/horatio_don', 'App\Http\Controllers\TgBotController@horatio_don');
+Route::get('/tg/horatio_don', 'App\Http\Controllers\TgBotController@horatio_don');
